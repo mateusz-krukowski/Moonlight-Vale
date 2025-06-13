941047bf7681ff48ef8f98489800c1769a72491b
Mateusz Krukowski <mateusz_krukowski@yahoo.com>
2025-06-13 02:33:04 +0200

    extracted dratTargetTile from devtools

--- source/MoonlightVale.cs
+++ source/MoonlightVale.cs
@@ -19,61 +19,60 @@ using Moonlight_Vale.Screens;
 */
 
 
-namespace Moonlight_Vale
+namespace Moonlight_Vale;
+
+public class MoonlightVale : Game
 {
-    public class MoonlightVale : Game
-    {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
-        private Desktop _desktop;
-        private ScreenManager _screenManager;
-        public FontSystem _fontSystem { get; private set; }
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Desktop _desktop;
+    private ScreenManager _screenManager;
+    public FontSystem _fontSystem { get; private set; }
         
-        private const byte WIDTH_RATIO = 16 , HEIGHT_RATIO = 9;
+    private const byte WIDTH_RATIO = 16 , HEIGHT_RATIO = 9;
 
-        public MoonlightVale()
-        {
-            _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Content");
-            Console.WriteLine(Content.RootDirectory);
-        }
+    public MoonlightVale()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Content");
+        Console.WriteLine(Content.RootDirectory);
+    }
 
-        protected override void Initialize()
-        {
-            base.Initialize();
-            ScreenManager.Initialize(this);
+    protected override void Initialize()
+    {
+        base.Initialize();
+        ScreenManager.Initialize(this);
 
-            Window.Title = "Moonlight Vale";
-            _graphics.PreferredBackBufferWidth = 1920;
-            _graphics.PreferredBackBufferHeight = 1080;
-            _graphics.ApplyChanges(); 
-            _graphics.ToggleFullScreen();
+        Window.Title = "Moonlight Vale";
+        _graphics.PreferredBackBufferWidth = 1920;
+        _graphics.PreferredBackBufferHeight = 1080;
+        _graphics.ApplyChanges(); 
+        _graphics.ToggleFullScreen();
 
-            MyraEnvironment.Game = this;
-            _desktop = new Desktop();
+        MyraEnvironment.Game = this;
+        _desktop = new Desktop();
 
-            _screenManager = ScreenManager.Instance;
-            _screenManager.AddScreen(new SplashScreen(this, _screenManager, _spriteBatch, _desktop, _fontSystem));
-        }
+        _screenManager = ScreenManager.Instance;
+        _screenManager.AddScreen(new SplashScreen(this, _screenManager, _spriteBatch, _desktop, _fontSystem));
+    }
 
-        protected override void LoadContent()
-        {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-            _fontSystem = new FontSystem();
-            string fontPath = Path.Combine( Content.RootDirectory, "Fonts", "CelticBitRegular.ttf");
-            _fontSystem.AddFont(System.IO.File.ReadAllBytes(fontPath));
-        }
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        _fontSystem = new FontSystem();
+        string fontPath = Path.Combine( Content.RootDirectory, "Fonts", "CelticBitRegular.ttf");
+        _fontSystem.AddFont(System.IO.File.ReadAllBytes(fontPath));
+    }
 
-        protected override void Update(GameTime gameTime)
-        {
-            base.Update(gameTime);
-            _screenManager.Update(gameTime);
-        }
+    protected override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+        _screenManager.Update(gameTime);
+    }
 
-        protected override void Draw(GameTime gameTime)
-        {
-            _screenManager.Draw(gameTime, _spriteBatch);
-            base.Draw(gameTime);
-        }
+    protected override void Draw(GameTime gameTime)
+    {
+        _screenManager.Draw(gameTime, _spriteBatch);
+        base.Draw(gameTime);
     }
 }
--- source/Screens/Maps/PlayerFarm.cs
+++ source/Screens/Maps/PlayerFarm.cs
@@ -5,56 +5,57 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Squared.Tiled;
 
-namespace Moonlight_Vale.Screens.Maps;
-
-public class PlayerFarm : IMap
+namespace Moonlight_Vale.Screens.Maps
 {
-    public Map TileMap { get; private set; }
-    public string PathToTileMap => @"\Tilemaps\player_farm_reduced.tmx";
-    public HashSet<int> PasableTileIds { get; set; }
-    public Vector2 PlayerSpawnPoint { get; set; }
-    public List<(int, int)> Portals { get; set; }
-    public OverworldScreen OverworldScreen { get; }
-
-    public Texture2D TileSet => TileMap.Tilesets.Values.First().Texture;
-
-    public PlayerFarm(OverworldScreen overworldScreen)
+    public class PlayerFarm : IMap
     {
-        OverworldScreen = overworldScreen;
-        PlayerSpawnPoint = new Vector2(950,700); // Example spawn point, adjust as needed
-        PasableTileIds = new HashSet<int>
+        public Map TileMap { get; private set; }
+        public string PathToTileMap => @"\Tilemaps\player_farm_reduced.tmx";
+        public HashSet<int> PasableTileIds { get; set; }
+        public Vector2 PlayerSpawnPoint { get; set; }
+        public List<(int, int)> Portals { get; set; }
+        public OverworldScreen OverworldScreen { get; }
+
+        public Texture2D TileSet => TileMap.Tilesets.Values.First().Texture;
+
+        public PlayerFarm(OverworldScreen overworldScreen)
         {
-            1, //grass
-            12, //dirt
-            114,115,116, //stairs
-            98,99,100,101,102, //house porch
-            34,36,38,40 //cliff shadows
-                
-        }; 
-
-    }
-
-    public void LoadContent(ContentManager content)
-    {
-        TileMap = Map.Load(content.RootDirectory  + PathToTileMap, content);
-    }
-
-    public void DrawMap(SpriteBatch spriteBatch)
-    {
-        int scaledTileSize = (int)(TileMap.TileWidth * OverworldScreen.Zoom);
-
-        foreach (var layer in TileMap.Layers.Values)
-        {
-            for (int y = 0; y < layer.Height; y++)
+            OverworldScreen = overworldScreen;
+            PlayerSpawnPoint = new Vector2(950,700); // Example spawn point, adjust as needed
+            PasableTileIds = new HashSet<int>
             {
-                for (int x = 0; x < layer.Width; x++)
-                {
-                    int tileIndex = layer.GetTile(x, y);
-                    var tileRect = new Rectangle();
-                    if (!TileMap.Tilesets.First().Value.MapTileToRect(tileIndex, ref tileRect)) continue;
+                1, //grass
+                12, //dirt
+                114,115,116, //stairs
+                98,99,100,101,102, //house porch
+                34,36,38,40 //cliff shadows
+                
+            }; 
 
-                    spriteBatch.Draw(TileSet, new Vector2(x, y) * scaledTileSize, tileRect,
-                        Color.White, 0, Vector2.Zero, OverworldScreen.Zoom, SpriteEffects.None, 0);
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            TileMap = Map.Load(content.RootDirectory  + PathToTileMap, content);
+        }
+
+        public void DrawMap(SpriteBatch spriteBatch)
+        {
+            int scaledTileSize = (int)(TileMap.TileWidth * OverworldScreen.Zoom);
+
+            foreach (var layer in TileMap.Layers.Values)
+            {
+                for (int y = 0; y < layer.Height; y++)
+                {
+                    for (int x = 0; x < layer.Width; x++)
+                    {
+                        int tileIndex = layer.GetTile(x, y);
+                        var tileRect = new Rectangle();
+                        if (!TileMap.Tilesets.First().Value.MapTileToRect(tileIndex, ref tileRect)) continue;
+
+                        spriteBatch.Draw(TileSet, new Vector2(x, y) * scaledTileSize, tileRect,
+                            Color.White, 0, Vector2.Zero, OverworldScreen.Zoom, SpriteEffects.None, 0);
+                    }
                 }
             }
         }
